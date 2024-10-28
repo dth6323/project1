cf824f21cc52af7f92ec2b7febd4b41d649dacc5
dth63 <dth6323@gmail.com>
2024-10-28 10:59:52 +0700

    first

--- src/main/webapp/app/entities/entity.routes.ts
+++ src/main/webapp/app/entities/entity.routes.ts
@@ -6,6 +6,26 @@ const routes: Routes = [
     data: { pageTitle: 'project1App.adminAuthority.home.title' },
     loadChildren: () => import('./admin/authority/authority.routes'),
   },
+  {
+    path: 'cccd',
+    data: { pageTitle: 'project1App.cCCD.home.title' },
+    loadChildren: () => import('./cccd/cccd.routes'),
+  },
+  {
+    path: 'type-violation',
+    data: { pageTitle: 'project1App.typeViolation.home.title' },
+    loadChildren: () => import('./type-violation/type-violation.routes'),
+  },
+  {
+    path: 'vehicle-registrations',
+    data: { pageTitle: 'project1App.vehicleRegistrations.home.title' },
+    loadChildren: () => import('./vehicle-registrations/vehicle-registrations.routes'),
+  },
+  {
+    path: 'violations',
+    data: { pageTitle: 'project1App.violations.home.title' },
+    loadChildren: () => import('./violations/violations.routes'),
+  },
   /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
 ];
 
